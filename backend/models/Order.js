b019f4ea6a81517e5c786d0a889640f0ed713dd7
ParthJindal23k <parthjindal23316@gmail.com>
2025-02-12 01:11:46 +0530

    Updated

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -14,8 +14,66 @@ const orderSchema = new mongoose.Schema({
                 type:mongoose.Schema.Types.ObjectId,
                 ref:'Product',
                 required:true
-            }
+            },
+            quantiy:{type:number, required:true},
+            price: {type:number , required:true}
         }
-    ]
+    ],
 
-})
+    shippingAddress:{
+        address:{
+            type:String,
+            required:true
+        },
+        city:{
+            type:String,
+            required:true
+        },
+
+        Pincode:{
+            type:String,
+            required:true
+        },
+        country:{
+            type:String,
+            required:true
+        }
+    },
+    paymentMethod:{
+        type:String,
+        required:true
+    },
+
+    paymentResult:{
+        id:String,
+        status:String,
+        update_time:String,
+        email_address:String
+    },
+    totalPrice:{
+        type:Number,
+        required:true,
+    },
+    isPaid:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    paidAt:{
+        type:Date
+    },
+    isDelivered:{
+        type:Boolean,
+        required:true,
+        default:false
+
+    },
+    deliveredOn:{
+        type:Date
+    }
+
+
+
+},{timestamps : true});
+
+module.exports = mongoose.model('Order',orderSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,24 @@ require("dotenv").config({path:'./.env'});
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(express.json());
-
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI).then(()=>console.log("MongoDb connected with the server")).catch(err=>console.log(err));
+const UserRoutes = require("./routes/UserRoutes")
+const ProductRoutes = require("./routes/ProductRoutes")
+const OrderRoutes = require("./routes/OrderRoutes")
 
-
+app.use(express.json());
 app.use(cors());
+
+
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log("MongoDb connected with the server")).catch(err=>console.log(err));
+
+
+app.use("/api/users",UserRoutes);
+app.use("/api/products",ProductRoutes);
+app.use("/api/orders",OrderRoutes);
+
+
 
 app.listen(process.env.PORT,() => {
     console.log("Server is started on the port")
